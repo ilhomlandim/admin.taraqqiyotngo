2f1b647e9d45667dc217f2ebb3551980c5cb7eec
zokirovjasur <jasurbek7682@gmail.com>
2025-01-30 21:09:47 +0500

    loading

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LayoutWrapper from "@/components/layoutWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <LayoutWrapper>{children}</LayoutWrapper>{" "}
       </body>
     </html>
   );
