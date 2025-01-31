f78802a9380d6a36844da7326133e4d8bb08a69e
mirzoboxu <mirzoulugbekkhudoyberdiyev@gmail.com>
2025-01-31 20:08:09 +0500

    Fix muslimbek

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,25 @@
-import React from "react";
-
-export default function page() {
-  return <div>page</div>;
-
 "use client";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
 import ProtectedRoute from "@/components/ProdectedRoute";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function DashboardPage() {
   const router = useRouter();
 
   const handleLogout = () => {
-    localStorage.removeItem("token"); 
-    router.push("/auth"); 
+    localStorage.removeItem("token");
+    router.push("/auth");
   };
 
   return (
     <ProtectedRoute>
       <div className="flex flex-col justify-center items-center h-screen bg-gray-900 text-white">
         <h1 className="text-3xl font-bold">Welcome to Dashboard 🎉</h1>
-        <Button onClick={handleLogout} className="mt-5 bg-red-600 hover:bg-red-700">
+        <Button
+          onClick={handleLogout}
+          className="mt-5 bg-red-600 hover:bg-red-700"
+        >
           Logout
         </Button>
       </div>
