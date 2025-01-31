b19ee49721f4822ec36143572d921363926aa2ad
mirzoboxu <mirzoulugbekkhudoyberdiyev@gmail.com>
2025-01-31 19:54:42 +0500

    Fix ogabekiy

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/layoutWrapper";
-
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
-        <LayoutWrapper>{children}</LayoutWrapper>{" "}
-
+        <Providers>
+          <LayoutWrapper>{children}</LayoutWrapper>
+        </Providers>
       </body>
     </html>
   );
